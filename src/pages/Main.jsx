ae517fe3ae7b8bcb5fa7aaa82c70cceb82f0c711
gahee <ogh010@naver.com>
2024-06-08 17:03:00 +0900

    public url 설정 및 퍼블리싱 진행중

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import sample01 from '../assets/img/sample01.jpg'
+import sample02 from '../assets/img/sample02.jpg'
+import sample04 from '../assets/img/smaple04.jpeg'
+import { useState } from 'react';
 
 const Main = () => {
-  return (
+    const [heart,setHeart] = useState('🩶');
+    const toggleHeart = () => {
+        setHeart(heart == '🩶' ? '🩷' : '🩶')
+    }
+    return (
     <>
         <header>
             <div className='logo'></div>
@@ -18,11 +26,77 @@ const Main = () => {
                 </ul>
             </section>
             <section className='mainBox'>
-                <ul>
-                    <li>
-                        <div></div>
-                    </li>
-                </ul>
+                <div className='mainList'>
+                    <article>
+                        <div className='top'>
+                            <div className='left'>
+                                <div className='circle'></div>
+                                <span>im_winter</span>
+                            </div>
+                            <div className="menu"></div>
+                        </div>
+                        <div className='content'>
+                            {/* 이미지 */}
+                            <img src={sample01} alt="" />
+                        </div>
+                        <div className='bottom'>
+                            <div className='icoBox'>
+                                <span className='heart' onClick={toggleHeart}>{heart}</span>
+                                <span className='comment'>💬</span>
+                            </div>
+                            <p>좋아요 8개</p>
+                            <p><b>im_winter</b>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                            <span className='gray'>댓글 4개 모두 보기</span>
+                            <span className='gray'>2024.06.08</span>
+                        </div>
+                    </article>
+                    <article>
+                        <div className='top'>
+                            <div className='left'>
+                                <div className='circle'></div>
+                                <span>im_winter</span>
+                            </div>
+                            <div className="menu"></div>
+                        </div>
+                        <div className='content'>
+                            {/* 이미지 */}
+                            <img src={sample02} alt="" />
+                        </div>
+                        <div className='bottom'>
+                            <div className='icoBox'>
+                                <span className='heart' onClick={toggleHeart}>{heart}</span>
+                                <span className='comment'>💬</span>
+                            </div>
+                            <p>좋아요 8개</p>
+                            <p><b>im_winter</b>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                            <span className='gray'>댓글 4개 모두 보기</span>
+                            <span className='gray'>2024.06.08</span>
+                        </div>
+                    </article>
+                    <article>
+                        <div className='top'>
+                            <div className='left'>
+                                <div className='circle'></div>
+                                <span>im_winter</span>
+                            </div>
+                            <div className="menu"></div>
+                        </div>
+                        <div className='content'>
+                            {/* 이미지 */}
+                            <img src={sample04} alt="" />
+                        </div>
+                        <div className='bottom'>
+                            <div className='icoBox'>
+                                <span className='heart' onClick={toggleHeart}>{heart}</span>
+                                <span className='comment'>💬</span>
+                            </div>
+                            <p>좋아요 8개</p>
+                            <p><b>im_winter</b>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                            <span className='gray'>댓글 4개 모두 보기</span>
+                            <span className='gray'>2024.06.08</span>
+                        </div>
+                    </article>
+                </div>
             </section>
         </main>
 
